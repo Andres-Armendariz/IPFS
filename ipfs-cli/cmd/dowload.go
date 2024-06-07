28e6106c339fe455d5c741e261b6e4ba1e725b3c
AndresEAB <andres.armendariz@epn.edu.ec>
2024-06-06 19:57:46 -0500

    export ipfs file

--- ipfs-cli/cmd/dowload.go
+++ ipfs-cli/cmd/dowload.go
@@ -8,7 +8,7 @@ import (
 	shell "github.com/ipfs/go-ipfs-api"
 )
 
-var outputPath = "/home/edvard/Documents/"
+var outputPath = "/home/aaioet/" //Cambiar el path donde se quiere descargar el archivo
 
 // DownloadFileFromCID downloads a file from IPFS using its CID and saves it locally
 func DownloadFile(cid string) error {
